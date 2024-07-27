4a329f9921be5bec0a41c426e312c70ca23145f1
AjithKumarMK <ajithkumarajithkumar2254@gmail.com>
2024-07-27 20:11:23 +0530

    a varaible Created

--- JavaApplication21/src/javaapplication21/JavaApplication21.java
+++ JavaApplication21/src/javaapplication21/JavaApplication21.java
@@ -16,7 +16,11 @@ public class JavaApplication21 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World ")
+        System.out.println("Hello World");
+        int a = 18 ;
+        if(a==18){
+            System.out.println(a);
+        }
     }
     
 }
